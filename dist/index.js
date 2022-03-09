9afbf04564d110643bee61413c1eda4402da8245
Armand <armandjvanrensburg@gmail.com>
2022-03-09 11:00:22 +0200

    add compiled code

--- dist/index.js
+++ dist/index.js
@@ -11556,56 +11556,64 @@ const core = __nccwpck_require__(4255)
 const github = __nccwpck_require__(6447)
 
 // Wait method
-const wait = time =>
-  new Promise(resolve => setTimeout(resolve, time * 1000))
+// const wait = time =>
+//   new Promise(resolve => setTimeout(resolve, time * 1000))
+//
+// // Setup Axios instance
+// const axiosInstance = axios.create({
+//   baseURL: "https://api.vercel.com",
+//   headers: {
+//     Authorization: `Bearer ${core.getInput("access_token", {
+//       required: true
+//     })}`
+//   }
+// })
+//
+// const awaitVercelDeployment = async () => {
+//   const sha = github.context.payload.head_commit.id
+//   const timeout = +core.getInput("wait_for") * 1000
+//   const endTime = new Date().getTime() + timeout
+//
+//   while (new Date().getTime() < endTime) {
+//     try {
+//       const response = axiosInstance.get('/v6/deployments', {
+//         params: {
+//           "meta-githubCommitSha": github.context.payload.head_commit.id,
+//           projectId: core.getInput('project_id', {
+//             required: true
+//           })
+//         }
+//       })
+//
+//       if(response?.deployments?.length > 0) {
+//         return response.deployments[0]
+//       }
+//     } catch (e) {
+//       await wait(2)
+//     }
+//   }
+//
+//   throw new Error(`Timeout reached before deployment for ${sha} was found.`)
+// }
+//
+// (async () => {
+//   try {
+//     const deployment = await awaitVercelDeployment()
+//
+//     console.log(deployment)
+//     core.setOutput("url", deployment.url)
+//   } catch (err) {
+//     core.setFailed(err.message)
+//   }
+// })()
 
-// Setup Axios instance
-const axiosInstance = axios.create({
-  baseURL: "https://api.vercel.com",
-  headers: {
-    Authorization: `Bearer ${core.getInput("access_token", {
-      required: true
-    })}`
-  }
-})
-
-const awaitVercelDeployment = async () => {
-  const sha = github.context.payload.head_commit.id
-  const timeout = +core.getInput("wait_for") * 1000
-  const endTime = new Date().getTime() + timeout
-
-  while (new Date().getTime() < endTime) {
-    try {
-      const response = axiosInstance.get('/v6/deployments', {
-        params: {
-          "meta-githubCommitSha": github.context.payload.head_commit.id,
-          projectId: core.getInput('project_id', {
-            required: true
-          })
-        }
-      })
-
-      if(response?.deployments?.length > 0) {
-        return response.deployments[0]
-      }
-    } catch (e) {
-      await wait(2)
-    }
-  }
-
-  throw new Error(`Timeout reached before deployment for ${sha} was found.`)
+try {
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`)
+} catch (error) {
+  core.setFailed(error.message)
 }
-
-(async () => {
-  try {
-    const deployment = await awaitVercelDeployment()
-
-    console.log(deployment)
-    core.setOutput("url", deployment.url)
-  } catch (err) {
-    core.setFailed(err.message)
-  }
-})()
 
 })();
 
